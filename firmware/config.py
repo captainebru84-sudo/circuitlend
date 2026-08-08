7a483298eec390a7c5f9decf2cd59a34b2be5a74
captainebru84-sudo <252604649+captainebru84-sudo@users.noreply.github.com>
2026-08-08 01:18:47 +0100

    Wire deployed Monad addresses into breaker config (pool, CLUSD, borrower)

--- firmware/config.py
+++ firmware/config.py
@@ -5,14 +5,14 @@ WIFI_PASSWORD = '1234567800'
 RPC_URL = 'https://testnet-rpc.monad.xyz'
 
 # CircuitLendPool on Monad testnet + this device's loan id
-POOL_ADDRESS = ''  # filled after deploy
+POOL_ADDRESS = '0x5538b0CF2f97e73cedD6349Ef50E299A95B70DdF'
 LOAN_ID = 1
 
 # Borrower wallet + settlement A-Token (CLUSD) for the CVI probe:
 # the device simulates a 1-unit self-transfer from the borrower; a frozen
 # A-Pass makes the token revert APassNotActive on-chain.
-BORROWER_ADDRESS = ''
-SETTLEMENT_TOKEN = ''
+BORROWER_ADDRESS = '0x2e2BA14F6784B72fE9874b41811193B5B0bdd0cA'
+SETTLEMENT_TOKEN = '0x09A050Eb813ddb0b1EEAE7bca83bc1becD04FA31'  # CLUSD
 
 RELAY_PIN = 23        # active-high: HIGH = relay energized = device powered
 POLL_SECONDS = 4
